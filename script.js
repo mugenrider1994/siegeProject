4cb1144785280c84f978b4c5f65020aaecca2afe
Megan Glorioso <meganglorioso2016@gmail.com>
2024-08-22 12:49:07 -0700

    dropdown menu

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggleButton = document.getElementById('theme-toggle');
+
+// Check for saved user preference or system preference
+const currentTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+document.documentElement.setAttribute('data-theme', currentTheme);
+
+toggleButton.addEventListener('click', () => {
+    const theme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+});
